121db5caeaefa96714f623f09842f78eb5b84161
Juan Alvarado <alva.juan79@gmail.com>
2016-01-28 13:45:49 -0800

    Building schemas to add user book listing to Mongo database.#4

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/books', function() {
+  console.log('connected to mongo books collection');
+})
+
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  phone: Number,
+  bookname: String,
+  bookauthor: String,
+  sellprice: Number
+});
+
+var Book = mongoose.model('Book', bookSchema);
+
+var bookOne = new Book({
+  firstname: "Juan",
+  lastname: "Alvarado",
+  email: "alva.juan79@gmail.com",
+  phone: 5622909405,
+  bookname: "Harry Potter Deathly Hallows",
+  bookauthor: "J.K. Rowling",
+  sellprice: 9.99
+})
+
+bookOne.save(function() {
+  console.log('bookOne saved')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.use(express.static('./'));
 
