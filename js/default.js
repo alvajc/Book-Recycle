ad35363c6a9bf69138cac01cae43c484657609c6
Juan Alvarado <alva.juan79@gmail.com>
2016-01-28 15:52:48 -0800

    Modified default.js file

--- js/default.js
+++ js/default.js
@@ -1,16 +1,5 @@
 $.validate({
   form: '#registration-form',
-  modules: 'location'
 });
 
-$.validate({
-  modules : 'security',
-  onModulesLoaded : function() {
-    // Bind card type to card number validator
-    $('#credit-card').on('change', function() {
-      var card = $(this).val();
-      $('input[name="creditcard_num"]').attr('data-validation-allowing', card);
-    });
-  }
-});
 
