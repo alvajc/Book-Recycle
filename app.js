fd4b36d52854373a59c1e20075ae830facfd0133
Juan Alvarado <alva.juan79@gmail.com>
2016-01-28 14:13:18 -0800

    Moved book schema to module.exports. #4

--- app.js
+++ app.js
@@ -2,51 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var Book = require("./js/book.js");
 
-mongoose.connect('mongodb://localhost/books', function() {
-  console.log('connected to mongo books collection');
-})
-
-var Schema = mongoose.Schema;
-
-var bookSchema = new Schema({
-  firstname: String,
-  lastname: String,
-  email: String,
-  phone: Number,
-  bookname: String,
-  bookauthor: String,
-  sellprice: Number
-});
-
-var Book = mongoose.model('Book', bookSchema);
-
-var bookOne = new Book({
-  firstname: "Juan",
-  lastname: "Alvarado",
-  email: "alva.juan79@gmail.com",
-  phone: 5622909405,
-  bookname: "Harry Potter Deathly Hallows",
+var bookTwo = new Book({
+  firstname: "John",
+  lastname: "Smith",
+  email: "email@gmail.com",
+  phone: 1234567890,
+  bookname: "Harry Potter",
   bookauthor: "J.K. Rowling",
   sellprice: 9.99
-})
-
-bookOne.save(function() {
-  console.log('bookOne saved')
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+bookTwo.save(function() {
+  console.log("bookTwo saved");
+});
 
 app.use(express.static('./'));
 
---
+++ js/book.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/books', function() {
+  console.log('connected to mongo books collection');
+})
+
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  phone: Number,
+  bookname: String,
+  bookauthor: String,
+  sellprice: Number
+});
+
+var Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
