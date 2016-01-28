eb977b52a6fba1e12f3b9e1677d1ed8f7dabd1cb
Juan Alvarado <alva.juan79@gmail.com>
2016-01-28 15:48:39 -0800

    Building post route for book listing. #4

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Book = require("./js/book.js");
+var bookListing = require("./js/bookListing.js");
 
-var bookTwo = new Book({
-  firstname: "John",
-  lastname: "Smith",
-  email: "email@gmail.com",
-  phone: 1234567890,
-  bookname: "Harry Potter",
-  bookauthor: "J.K. Rowling",
-  sellprice: 9.99
-});
-
-bookTwo.save(function() {
-  console.log("bookTwo saved");
-});
-
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('./'));
+
+var bookListing = app.route('/bookListing');
+
+bookListing.post(function(req, res) {
+  var book = new Book();
+  book.firstname = req.body.firstname;
+  book.lastname = req.body.lastname;
+  book.email = req.body.email;
+  book.phone = req.body.phone;
+  book.bookname = req.body.bookname;
+  book.bookauthor = req.body.bookauthor;
+  book.sellprice = req.body.sellprice;
+  book.save();
+});
 
 app.listen(1337);
 console.log('listening on localhost:1337');
